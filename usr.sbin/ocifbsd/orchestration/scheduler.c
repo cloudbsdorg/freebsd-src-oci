e9cb6403bda67943d8dfed07281dab01c0e0c03f
Mark LaPointe <mark@cloudbsd.org>
2026-06-04 21:21:04 -0500

    oci-bootstrap: add static to 3 file-local scheduler functions

--- usr.sbin/ocifbsd/orchestration/scheduler.c
+++ usr.sbin/ocifbsd/orchestration/scheduler.c
@@ -453,7 +453,7 @@ scheduler_node_not_ready(const char *node_name)
 /*
  * Update node resources (called by metrics collector)
  */
-int
+static int
 scheduler_update_node_resources(const char *node_name, 
     uint64_t memory_used, uint64_t cpu_used)
 {
