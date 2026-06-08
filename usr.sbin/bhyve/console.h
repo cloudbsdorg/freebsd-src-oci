47db57117e3475d72fd53093b56a6f25a8b3f915
displayd-build-agent <displayd@freebsd-src-oci.local>
2026-06-08 14:38:57 -0500

    bhyve(display): add per-instance console API to console.h (T8.A)

--- usr.sbin/bhyve/console.h
+++ usr.sbin/bhyve/console.h
@@ -35,6 +35,40 @@ typedef void (*fb_render_func_t)(struct bhyvegc *gc, void *arg);
 typedef void (*kbd_event_func_t)(int down, uint32_t keysym, uint32_t keycode, void *arg);
 typedef void (*ptr_event_func_t)(uint8_t mask, int x, int y, void *arg);
 
+#include <sys/queue.h>
+
+struct console_ctx;
+
+#define CONSOLE_FB_RAW    0x0001  /* skip bhyvegc_init (jail path) */
+#define CONSOLE_FB_BHYVEGC 0x0000  /* default: bhyvegc enabled */
+
+struct console_ctx *console_create(int w, int h, void *fbaddr, int flags);
+void                console_destroy(struct console_ctx *ctx);
+struct console_ctx *console_get_default(void);
+
+void                console_set_fbaddr_ctx(struct console_ctx *ctx, void *fbaddr);
+struct bhyvegc_image *console_get_image_ctx(struct console_ctx *ctx);
+
+void                console_fb_register_ctx(struct console_ctx *ctx,
+                              fb_render_func_t render_cb, void *arg, int pri);
+int                 console_fb_unregister_ctx(struct console_ctx *ctx,
+                                fb_render_func_t render_cb);
+void                console_refresh_ctx(struct console_ctx *ctx);
+
+void                console_kbd_register_ctx(struct console_ctx *ctx,
+                               kbd_event_func_t event_cb, void *arg, int pri);
+int                 console_kbd_unregister_ctx(struct console_ctx *ctx,
+                                 kbd_event_func_t event_cb);
+void                console_key_event_ctx(struct console_ctx *ctx,
+                             int down, uint32_t keysym, uint32_t keycode);
+
+void                console_ptr_register_ctx(struct console_ctx *ctx,
+                               ptr_event_func_t event_cb, void *arg, int pri);
+int                 console_ptr_unregister_ctx(struct console_ctx *ctx,
+                                 ptr_event_func_t event_cb);
+void                console_ptr_event_ctx(struct console_ctx *ctx,
+                             uint8_t button, int x, int y);
+
 void console_init(int w, int h, void *fbaddr);
 
 void console_set_fbaddr(void *fbaddr);
